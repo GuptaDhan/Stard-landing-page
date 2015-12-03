e4bce65b0c093f07e42b5cf5d5227fe950636f46
Jay Kumar <jay.sivan.kumar@gmail.com>
2015-12-03 14:42:06 -0800

    Second version of landing page

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var howitworksbutton = document.getElementById('howitworksbutton');
+
+// function showhowitworks (){
+//   var howitworks = document.getElementById('howitworks');
+//   howitworks.setAttribute('class', 'show')
+// }
+
+howitworksbutton.addEventListener('click', function(){
+  var howitworks = document.getElementById('howitworks');
+  howitworks.setAttribute('class', 'show');
+});
